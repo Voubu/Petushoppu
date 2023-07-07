8bd3122fb360c9fdafa5df75c63222cadedb397f
unknown <aryashogun@gmail.com>
2023-07-07 15:40:18 +0700

    Lil bit change for the login page visual

--- admin/login.php
+++ admin/login.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Petshop</title>
+    <title>Petshop - Login Admin</title>
     <link rel="icon" href="../img/logo.png" type="image/x-icon">
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -18,8 +18,8 @@
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
 </head>
-<?php
 
+<?php
 session_start();
 include '../functions.php';
 
@@ -53,50 +53,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<body>
+<body class="bg-gradient-primary">
 
     <div class="container">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-            <div class="d-flex flex-column">
-                <h4 class="mt-5 text-center">Petshop - Login Admin</h4>
-                <div class="d-flex justify-content-center">
-                    <img src="../img/logo.png" class="img-fluid" width="100" alt="">
-                </div>
-            </div>
-            <div class="col-xl-10 col-lg-12 col-md-9 col-sm-12">
+        <div class="row justify-content-center align-items-center vh-100">
+            <div class="col-lg-6">
 
-                <div class="card o-hidden border-0 my-5">
+                <div class="card o-hidden border-0 shadow-lg my-5 rounded-lg">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col">
-                                <div class="p-2">
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Petshop - Login Admin</h1>
+                                        <img src="../img/logo.png" class="img-fluid mb-4" width="100" alt="">
+                                    </div>
                                     <form class="user" method="POST" action="">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" name="username" placeholder="Masukkan Username" required>
+                                            <input type="text" class="form-control form-control-user rounded-pill bg-light py-3 px-4" name="username" placeholder="Username" required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" name="password" placeholder="Masukkan Password" required>
+                                            <input type="password" class="form-control form-control-user rounded-pill bg-light py-3 px-4" name="password" placeholder="Password" required>
                                         </div>
-                                        <button type="submit" name="login" class="btn btn-primary btn-user btn-block">
+                                        <button type="submit" name="login" class="btn btn-primary btn-user btn-block rounded-pill">
                                             Login
                                         </button>
                                     </form>
+                                    <?php if (isset($error)) { ?>
+                                        <div class="text-center mt-3">
+                                            <span class="text-danger"><?= $error; ?></span>
+                                        </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
 
     </div>
 
-    <?php include "footer.php"; ?>
+    <footer class="text-center py-3 fixed-bottom bg-white">
+        <div class="container">
+            <p class="m-0">Copyright &copy; Petshop <?= date('Y'); ?></p>
+        </div>
+    </footer>
+
     <?php include "plugin.php"; ?>
 
 </body>
